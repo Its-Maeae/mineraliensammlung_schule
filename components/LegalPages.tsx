d52052fafcdf8e483654aa366d0c1fc7fc6e843d
Its-Maeae <mariusschmi1711@gmail.com>
2025-08-29 22:36:12 +0200

    rechtschreibung fix

--- components/LegalPages.tsx
+++ components/LegalPages.tsx
@@ -38,7 +38,7 @@ export default function LegalPages({ currentPage }: LegalPagesProps) {
             
             <div className="legal-section">
               <h2>Haftung für Inhalte</h2>
-              <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und AktualitÃ¤t der Inhalte können wir jedoch keine Gewähr Übernehmen.</p>
+              <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr Übernehmen.</p>
             </div>
             
             <div className="legal-section">
